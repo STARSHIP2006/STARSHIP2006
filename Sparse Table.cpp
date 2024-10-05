3b765f974279d760c5cf157b0ffbc77020b2e0cf
STARSHIP2006 <srutarshi.iitkgp.cse@gmail.com>
2024-10-05 22:03:15 +0530

    Create Sparse Table.cpp

---
+++ Sparse Table.cpp
@@ -0,0 +1,14 @@
+# define N 1000
+int lookup[N][N];
+void buildSparseTable(int arr[], a,b, int n) 
+{ 
+    // Initialize M for the intervals with length 1 
+    for(int i=1; i<=n; i++)
+  
+    for(int j=1; (1<<j)<=n; j++)
+        continue;
+    j--;
+    for(int i=1; j
+    lookup[a][b]=mi
+} 
+ 
