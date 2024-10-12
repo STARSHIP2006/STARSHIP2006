0669d56b128681916e68e659994718655f13b8f2
STARSHIP2006 <srutarshi.iitkgp.cse@gmail.com>
2024-10-12 12:28:26 +0530

    Update Important number theory functions.cpp

--- Important number theory functions.cpp
+++ Important number theory functions.cpp
@@ -16,3 +16,21 @@ int gcd (int a, int b)
   if(b%a==0) return a;
   else return gcd(b%a, a);
 }
+
+//sieve of eratorthenos
+vector<int> factors (int n)
+{
+    vector<bool> prime(n + 1, true);
+    vector<int> v;
+    for (int p = 2; p * p <= n; p++) 
+    {
+        if (prime[p] == true) {
+            for (int i = p * p; i <= n; i += p)
+                prime[i] = false;
+        }
+    }
+    for (int p = 2; p <= n; p++)
+        if (prime[p])
+            v.push_back(p);
+    return v;//contains all primes <=n
+}
